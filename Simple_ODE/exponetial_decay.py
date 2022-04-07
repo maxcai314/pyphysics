2fcaa4b52a2811955ed1b4bdbc1495048ccc3a61
Max Cai <maxcai.paloalto@gmail.com>
2022-04-07 15:39:25 -0700

    testing out numpy and matplotlib, plot analytic solution

--- Simple_ODE/exponetial_decay.py
+++ Simple_ODE/exponetial_decay.py
@@ -6,4 +6,17 @@ Created on Thu Apr  7 15:21:14 2022
 @author: maxcai
 """
 
-print("hello world")
+import numpy as np
+import matplotlib.pyplot as plt
+
+lam = 0.5
+y0 = 10
+dt=0.1
+t = np.arange(0, 10, dt)
+y = y0 * np.exp(-lam * t)
+
+#plotting
+plt.plot(t, y)
+plt.xlabel("t")
+plt.ylabel("y")
+plt.show()
