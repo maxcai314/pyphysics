0d68292415a6d821ddce675feb5231a3f485a331
Max Cai <maxcai.paloalto@gmail.com>
2022-06-05 17:27:31 -0700

    Animated Pendulum with Frog Image. Framerate is toggleable.

--- Simple_ODE/single_pendulum_ef_animate.py
+++ Simple_ODE/single_pendulum_ef_animate.py
@@ -13,12 +13,12 @@ import sys
 #dd_theta + g/l sin(theta) = 0
 
 g = 10
-l = 10
+l = 5
 m = 1
 dt = 1E-3
-theta0 = 3.1
+theta0 = 1
 omega0 = 0
-t = np.arange(0, 40, dt)
+t = np.arange(0, 20, dt)
 theta = np.zeros_like(t)
 omega = np.zeros_like(t)
 energy = np.zeros_like(t)
@@ -38,18 +38,23 @@ print("Energy change = ", energy[-1] - energy[0])
 
 
 class Pendulum(pygame.sprite.Sprite):
-    def __init__(self,window):
+    def __init__(self,window, theta):
         super().__init__()
         self.window = window
         print("Successfully created pendulum")
         self.image = pygame.image.load("frog/attack_1.gif")
         self.rect = self.image.get_rect()
-        self.rect.topleft = [200,200]
-    def update(self,theta):
-        #pygame.draw.circle(self.window, (255,0,0), (200,200),50)
-        print(theta, "is the theta given to pendulum")
-        pass
+        self.rect.topleft = (0,0)
+        self.theta = theta
+    def update(self,timestep):
+        #pygame.draw.circle(self.window, (255,0,0), (x,y),5)
+        r = 0.3 * min(self.window.get_size())
+        x = 0.5 * self.window.get_size()[0] + r * np.sin(self.theta[int(timestep)])
+        y = 0.5 * self.window.get_size()[1] + r * np.cos(self.theta[int(timestep)])
         
+        self.rect.topleft = [x,y]
+        pygame.draw.line(self.window, (0,0,255), (200,200),(x,y),2)
+        #Use return statmement for double pendulum coordinate stacking
         
 
 # General setup
@@ -62,19 +67,30 @@ screen_height = 400
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Pendulum Animation")
 
+print(screen.get_size())
 # Creating the sprites and groups
 moving_sprites = pygame.sprite.Group()
-pendulum1 = Pendulum(screen)
+pendulum1 = Pendulum(screen, theta)
 moving_sprites.add(pendulum1)
 
+
+fps = 20
+timestep = 0
 while True:
     for event in pygame.event.get():   
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    
-    moving_sprites.update(10)
+    screen.fill((0,0,0))
+    moving_sprites.update(timestep)
     moving_sprites.draw(screen)
     pygame.display.flip()
     
-exit()
+    print("index ", timestep)
+    print("time ", t[int(timestep)])
+    clock.tick(fps)
+    timestep += 1/fps/dt
+    if timestep >= np.size(t):
+        pygame.quit()
+        sys.exit()
+    
