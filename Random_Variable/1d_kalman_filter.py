ed0b124347cf57add7535168dbcb2ff6d459c222
Max Cai <maxcai.paloalto@gmail.com>
2022-07-02 13:00:39 +0100

    calculated and compared formula with Kalman Gain on a given random seed

--- Random_Variable/1d_kalman_filter.py
+++ Random_Variable/1d_kalman_filter.py
@@ -19,10 +19,14 @@ B = 1
 C = 1
 x = np.zeros(N)
 u = np.zeros(N) #Control
+np.random.seed(seed=10)
 epsilon = np.random.normal(0,np.sqrt(R),N)
 delta = np.random.normal(0,np.sqrt(Q),N)
 mu = np.zeros(N)
 Sigma = np.zeros(N)
+mu_k = np.zeros(N)
+Sigma_k = np.zeros(N)
+
 for i in range(N):
     u[i] = 0.1
 
@@ -40,7 +44,9 @@ for i in range(1,N):
 
 #kalman filter
 mu[0] = x0
+mu_k[0] = x0
 Sigma[0] = Sigma0
+Sigma_k[0] = Sigma0
 for i in range (1,N):
     #prediction
     mu_pred = A * mu[i-1] + B * u[i]
@@ -49,7 +55,23 @@ for i in range (1,N):
     Sigma[i] = 1./(C * 1./Q * C + 1./Sigma_pred)
     mu[i] = Sigma[i] * (C * 1./Q * z[i] + 1./Sigma_pred * mu_pred)
     
-    
+#kalman filter (kalman gain)
+mu[0] = x0
+Sigma[0] = Sigma0
+for i in range (1,N):
+    #prediction
+    mu_pred = A * mu[i-1] + B * u[i]
+    Sigma_pred = A * Sigma[i-1] * A + R
+    #kalman gain
+    K = Sigma_pred * C * 1./(C * Sigma_pred * C + Q)
+    Sigma_k[i] = (1 - K * C) * Sigma_pred
+    mu_k[i] = mu_pred + K * (z[i] - C * mu_pred)
+    #measurement
+    Sigma[i] = 1./(C * 1./Q * C + 1./Sigma_pred)
+    mu[i] = Sigma[i] * (C * 1./Q * z[i] + 1./Sigma_pred * mu_pred)
+
+#np.max(np.abs(mu_k - mu))
+#np.max(np.abs(Sigma_k - Sigma))
 
 plt.figure(1)
 plt.plot(t,x,'r',label="Real State")
