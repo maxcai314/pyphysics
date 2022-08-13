40a783fe6386138c8a49201abf1465545e1bec5b
Max Cai <maxcai.paloalto@gmail.com>
2022-08-12 18:34:14 -0700

    implemented functions into Robot class

--- Mecanum_Drive/mecanum_drive.py
+++ Mecanum_Drive/mecanum_drive.py
@@ -24,12 +24,19 @@ class Robot():
         self.alpha = alpha
         self.friction=friction
         
+        self.M_r = np.diag([self.m,self.m,self.I_z])
+        self.M_w = np.diag(self.I_w)
+        
         if self.S==None:
             self.S = [self.L, self.L, -self.L, -self.L]
         if self.d==None:
             self.d = [self.l, -self.l, self.l, -self.l]
         if self.alpha==None:
             self.alpha = [0.25 * np.pi, -0.25 * np.pi, -0.25 * np.pi, 0.25 * np.pi]
+        
+        self.R = np.zeros((4,3))
+        for i in range(4):
+            self.R[i] = 1/self.r * np.array([1, -np.tan(self.alpha[i])**-1, -self.d[i]-self.S[i]*(np.tan(self.alpha[i])**-1)])
     
     def rotationmatrix(psi):
         return np.array([[np.cos(psi),-np.sin(psi),0],[np.sin(psi),np.cos(psi),0],[0,0,1]])
@@ -37,27 +44,75 @@ class Robot():
     def rotationmatrixdot(psi,psidot):
         return np.array([[-np.sin(psi),-np.cos(psi),0],[np.cos(psi),-np.sin(psi),0],[0,0,0]]) * psidot
     
-    def H_analytic():
-        #print(I_w[0] * 4 * r**-2 * q_rdot[2,0,-1])
-        #print(m+I_w[0] * 4 * r**-2)
-        #print(I_z+ I_w[0] * 4 * (l+L)**2 * r**-2)
-        pass
+    def H_analytic(self):
+        return np.diag([self.m+self.I_w[0] * 4 * self.r**-2, self.m+self.I_w[0] * 4 * self.r**-2,self.I_z+ self.I_w[0] * 4 * (self.l+self.L)**2 * self.r**-2])
     
-    def K_div_psidot_analytic():
-        pass
+    def K_div_psidot_analytic(self):
+        return np.matrix([[0,self.I_w[0] * 4 * self.r**-2 ,0],[self.I_w[0] * 4 * self.r**-2,0,0],[0,0,0]])
     
-    def time_integrate(self,N,dt):
+    def time_integrate(self,N,dt=1E-2,Gamma=None,q_r0 = np.array([[0],[0],[0]]),q_rdot0 = np.array([[0],[0],[0]])):
         self.N = N
         self.dt = dt
-        pass
+        self.t = np.arange(0, self.N*self.dt, self.dt)
+        
+        if (Gamma == None):
+            Gamma = np.zeros((4,1,N))
+        
+        q_r0 = np.array([[0],[0],[0]])
+        q_rdot0 = np.array([[1],[0],[2]])
+        self.q_r = np.zeros((3,1,N))
+        self.q_rdot = np.zeros((3,1,N))
+        self.q_r[:,:,0] = q_r0
+        self.q_rdot[:,:,0] = q_rdot0
+        
+        for i in range(1,N):
+            Rotation = rotationmatrix(self.q_r[2,0,i-1])
+            Rotationdot = rotationmatrixdot(self.q_r[2,0,i-1],self.q_rdot[2,0,i-1])
+            
+            self.H = self.M_r + Rotation @ self.R.T @ self.M_w @ self.R @ Rotation.T
+            self.K = Rotation @ self.R.T @ self.M_w @ self.R @ Rotationdot.T
+            self.F_a = Rotation @ self.R.T @ Gamma[:,:,i-1]
+            
+            self.q_rddot = np.linalg.inv(self.H) @ (self.F_a - self.K @ self.q_rdot[:,:,i-1] - self.friction * self.q_rdot[:,:,i-1])
+            self.q_rdot[:,:,i] = self.q_rdot[:,:,i-1] + self.q_rddot * self.dt
+            self.q_r[:,:,i] = self.q_r[:,:,i-1] + self.q_rdot[:,:,i-1] * self.dt
+        
     
-    def plot_evolution():
-        pass
+    def plot_evolution(self):
+        plt.plot([0, np.max(self.t)],[0,0],'k')
+        plt.plot(self.t, self.q_r[0,0,:],'b', label='X position')
+        plt.plot(self.t, self.q_r[1,0,:],'r', label='Y position')
+        plt.plot(self.t, self.q_r[2,0,:],'g', label='Psi position')
+        plt.legend()
+        plt.xlabel('t')
+        plt.title('Mechanum Wheeled Robot')
+        plt.show()
+        
+        plt.plot([0, np.max(self.t)],[0,0],'k')
+        plt.plot(self.t, self.q_rdot[0,0,:],'b', label='X velocity')
+        plt.plot(self.t, self.q_rdot[1,0,:],'r', label='Y velocity')
+        plt.plot(self.t, self.q_rdot[2,0,:],'g', label='Psi velocity')
+        plt.legend()
+        plt.xlabel('t')
+        plt.title('Robot Velocity')
+        plt.show()
     
-    def plot_trajectory():
-        pass
-    
+    def plot_trajectory(self):
+        xPos = self.q_r[0,0,:]
+        yPos = self.q_r[1,0,:]
 
+        maxd = np.max(np.abs(np.array([xPos,yPos])))
+        
+        plt.axes().set_aspect('equal')
+        axis = plt.gca()
+        axis.set_aspect('equal')
+        plt.xlim(-1.1*maxd,1.1 * maxd)
+        plt.ylim(-1.1*maxd,1.1 * maxd)
+        plt.plot(xPos,yPos,'b')
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.title('Robot Trajectory')
+        
 robot = Robot()
 
 N = int(1E3)
@@ -143,7 +198,7 @@ plt.title('Robot Velocity')
 plt.show()
 
 plt.figure(3)
-#plt.axes().set_aspect('equal')
+plt.axes().set_aspect('equal')
 axis = plt.gca()
 axis.set_aspect('equal')
 plt.xlim(-1.1*maxd,1.1 * maxd)
