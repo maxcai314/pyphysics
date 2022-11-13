0ab942875d2ed8ae455cba63514f42878c95646d
Max Cai <maxcai.paloalto@gmail.com>
2022-11-12 20:34:36 -0800

    added case to test if the robot is able to calculate the correct motor powers given its center of mass offset, and didn't seem to solve the issue with moving sideways. This could potentially be an issue with the dynamics model.

--- Mecanum_Drive/test_center_of_mass.py
+++ Mecanum_Drive/test_center_of_mass.py
@@ -20,22 +20,36 @@ q_r0 = np.array([[0],[0],[0]])
 q_rdot0 = np.array([[0],[0],[0]])
 robot_ideal.time_integrate(q_r0, q_rdot0, Gamma, N)
 
-
 robot_real = Robot(m=100.,I_z=200.,x_center=0.1,y_center=0)
-q_r0 = np.array([[0],[0],[0]])
-q_rdot0 = np.array([[0],[0],[0]])
-robot_real.time_integrate(q_r0, q_rdot0, Gamma, N)
+Gamma_real = Gamma
 # robot isn't aware that the calculations aren't accurate to its real center of mass
 # uses ideal calculation of Gamma
+q_r0 = np.array([[0],[0],[0]])
+q_rdot0 = np.array([[0],[0],[0]])
+robot_real.time_integrate(q_r0, q_rdot0, Gamma_real, N)
+
+robot_real_adjusted = Robot(m=100.,I_z=200.,x_center=0.1,y_center=0)
+Gamma_adjusted = np.zeros((N,4,1))
+for i in range(N):
+    Gamma_adjusted[i] = robot_real_adjusted.control_heading_to_torque(np.array([[0.],[0.1],[0.]]))
+q_r0 = np.array([[0],[0],[0]])
+q_rdot0 = np.array([[0],[0],[0]])
+robot_real_adjusted.time_integrate(q_r0, q_rdot0, Gamma_adjusted, N)
+# robot uses adjuted calculations, should be accurate
 
 trajectory = plt.figure()
 robot_ideal.plot_trajectory(fig=trajectory, show=False, linecolor='b', label="ideal")
 robot_real.plot_trajectory(fig=trajectory, show=False, linecolor='g', label="real")
+robot_real_adjusted.plot_trajectory(fig=trajectory, show=False, linecolor='m', label="real improved")
 plt.show()
 
-print("ideal:")
+print("\nideal:")
 print(robot_ideal.q_r[-1])
 print("\nreal:")
 print(robot_real.q_r[-1])
+print("\nreal improved:")
+print(robot_real_adjusted.q_r[-1])
 print("\ndifference:")
 print(robot_real.q_r[-1]-robot_ideal.q_r[-1])
+print("\ndifference with improvement:")
+print(robot_real_adjusted.q_r[-1]-robot_ideal.q_r[-1])
