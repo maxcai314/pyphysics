374e547fad2220d6368bf7da19c53553ea3d1284
Alec Petridis <chop@quenda.net>
2022-12-18 13:26:42 -0800

    Playboy genious billionar

---
+++ Mecanum_Drive/motor_comparison.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+from motor_simulation import Motor
+import pandas as pd
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    df = pd.read_csv('neverest_with_mecanum_wheel.csv').to_dict('records')
+
+    # motor_consts = []
+    # errors = []
+    # for motor_const in np.arange(0.3, 0.4, .01):
+    #     orbital_20 = Motor(.005, 1.6, motor_const, 0.37)
+    #
+    #     time = np.zeros(len(df) - 1)
+    #     angular_vel_real = np.zeros(len(df) - 1)
+    #     angular_vel_pred = np.zeros(len(df) - 1)
+    #
+    #     for index in range(0, len(df) - 1):
+    #         time[index] = df[index]['time']
+    #         angular_vel_real[index] = df[index]['motor_velocity']
+    #         angular_vel_pred[index] = orbital_20.angular_vel
+    #
+    #         orbital_20.time_integrate(df[index]['m_power'] * df[index]['batt_voltage'], df[index + 1]['time'] - df[index]['time'])
+    #
+    #     motor_consts.append(motor_const)
+    #     errors.append(np.average((angular_vel_real - angular_vel_pred)))
+    #
+    # plt.figure()
+    # plt.plot(motor_consts, errors)
+    # plt.xlabel('motor_const')
+    # plt.ylabel('error (rad/s)')
+    # plt.title("Simulation error")
+    # plt.legend()
+    # plt.show()
+
+    orbital_20 = Motor(.005, 1.6, 0.36, 0.37)
+    time = np.zeros(len(df) - 1)
+    angular_vel_real = np.zeros(len(df) - 1)
+    angular_vel_pred = np.zeros(len(df) - 1)
+
+    for index in range(0, len(df) - 1):
+        time[index] = df[index]['time']
+        angular_vel_real[index] = df[index]['motor_velocity']
+        angular_vel_pred[index] = orbital_20.angular_vel
+
+        orbital_20.time_integrate(df[index]['m_power'] * df[index]['batt_voltage'], df[index + 1]['time'] - df[index]['time'])
+
+
+    plt.figure()
+    plt.plot(time, angular_vel_real - angular_vel_pred, 'g', label="error")
+    plt.plot(time, angular_vel_real, 'r', label="real")
+    plt.plot(time, angular_vel_pred, 'b', label="pred")
+    plt.xlabel('time')
+    plt.ylabel('error (rad/s)')
+    plt.title("Simulation error")
+    plt.legend()
+    plt.show()
--- Mecanum_Drive/motor_simulation.py
+++ Mecanum_Drive/motor_simulation.py
@@ -8,57 +8,83 @@ Created on Sun Dec 18 10:35:09 2022
 
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy import floor
+
 
 class Motor:
-    # INDUCTANCE = 0
-    MOMENT_OF_INERTIA = 2
-    ARMATURE_RESISTANCE = 1.6
-    TORQUE_CONSTANT = 0.366
-    EMF_CONSTANT = 0.366
-    VISCOUS_FRICTION_COEFFICIENT = 0.04
-    
-    def __init__(self):
+    def __init__(self, moment_of_inertia, armature_resistance, motor_constant, viscous_friction):
+        self.moment_of_inertia = moment_of_inertia
+        self.armature_resistance = armature_resistance
+        self.motor_constant = motor_constant
+        self.viscous_friction = viscous_friction
+
         self.torque = 0
         self.angular_vel = 0
-    
-    def torque_dot(self,voltage):
-        return ((self.VISCOUS_FRICTION_COEFFICIENT * self.TORQUE_CONSTANT) * voltage + (self.VISCOUS_FRICTION_COEFFICIENT * self.ARMATURE_RESISTANCE - self.EMF_CONSTANT * self.TORQUE_CONSTANT) * self.torque)/(self.ARMATURE_RESISTANCE * self.MOMENT_OF_INERTIA)
-    
+        self.last_voltage = 0
+
+    # [diff(T(t), t) == (B_m*K_T*e_a(t) + J_m*K_T*diff(E_a(s), s) - (K_T^2 + B_m*R_a)*T(t))/(J_m*R_a)]
+    def torque_dot(self, voltage, voltage_dot):
+        return (
+                (self.viscous_friction * self.motor_constant * voltage) +
+                (self.moment_of_inertia * self.motor_constant * voltage_dot) -
+                (self.motor_constant * self.motor_constant + self.viscous_friction * self.armature_resistance) * self.torque
+        ) / (self.moment_of_inertia * self.armature_resistance)
+
+    # (K_T*E_a(t) - (K_T^2 + B_m*R_a)*O(t))/(J_m*R_a)
     def angular_vel_dot(self, voltage):
-        return ((self.EMF_CONSTANT * voltage) - (self.VISCOUS_FRICTION_COEFFICIENT * self.ARMATURE_RESISTANCE) * self.angular_vel)/(self.MOMENT_OF_INERTIA * self.ARMATURE_RESISTANCE)
-    
-    def time_integrate(self, voltage, time_step):
-        torque_dot = self.torque_dot(voltage)
+        return (
+                self.motor_constant * voltage -
+                (self.motor_constant ** 2 + self.viscous_friction * self.armature_resistance) * self.angular_vel
+        ) / (self.moment_of_inertia * self.armature_resistance)
+
+    def time_integrate_(self, voltage, time_step):
+        torque_dot = self.torque_dot(voltage, (voltage - self.last_voltage) / time_step)
         angular_vel_dot = self.angular_vel_dot(voltage)
-        
+
         self.torque += torque_dot * time_step
         self.angular_vel += angular_vel_dot * time_step
-    
+        self.last_voltage = voltage
+    def time_integrate(self, voltage, time_step):
+        if time_step < .01:
+            self.time_integrate_(voltage, time_step)
+        else:
+            num_steps = time_step / .01
+            curr_time = 0
+            for i in range(int(num_steps)):
+                self.time_integrate_(voltage, .01)
+                curr_time += .01
+
+            self.time_integrate_(voltage, time_step - curr_time)
+
+
     def get_state(self):
-        return np.array([[self.angular_vel],[self.torque]])
-    
+        return np.array([[self.angular_vel], [self.torque]])
+
     def to_string(self):
         return f'Angular Velocity {self.angular_vel}, Torque {self.torque}'
 
+
 if __name__ == "__main__":
-    motor = Motor()
-    
-    N = 100000
+    motor = Motor(.01, 1.6, .366, 0)
+
+    total_time = .5
     dt = 0.005
-    simulation_time = np.arange(0,N)*dt
-    state = np.zeros((N,2,1))
-    for i in range(0,100000):
+    N = int(total_time / dt)
+
+    simulation_time = np.arange(0, N) * dt
+    state = np.zeros((N, 2, 1))
+    for i in range(0, N):
         state[i] = motor.get_state()
-        motor.time_integrate(12, dt)
+        motor.time_integrate(6, dt)
         print(motor.to_string())
-    
+
     # for i in range(50000,N):
     #     state[i] = motor.get_state()
     #     motor.time_integrate(0, dt)
-    
+
     plt.figure()
-    angular_vel = state[:,0,0]
-    torque = state[:,1,0]
+    angular_vel = state[:, 0, 0]
+    torque = state[:, 1, 0]
     plt.plot(simulation_time, angular_vel, 'r', label="angular velocity")
     plt.plot(simulation_time, torque, 'b', label="torque")
     plt.xlabel('time')
