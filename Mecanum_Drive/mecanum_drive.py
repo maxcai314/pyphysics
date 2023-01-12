09631f7c91beee86eaf2ff51d17688e1feacd0e8
Max Cai <maxcai.paloalto@gmail.com>
2023-01-11 22:17:26 -0800

    changed methods to be able to simulate step by step

--- Mecanum_Drive/mecanum_drive.py
+++ Mecanum_Drive/mecanum_drive.py
@@ -17,7 +17,7 @@ class Robot():
                  I_w=[0.05,0.05,0.05,0.05],
                  S_geometric=None,d_geometric=None,alpha=None,
                  x_center=0., y_center=0.,
-                 friction=0.1):
+                 friction=0.1, q_r=None, q_rdot=None, d1=0.1,d2=-0.1,S3=-0.1,r_odo=0.05):
         self.L=L
         self.l=l
         self.r=r
@@ -28,6 +28,12 @@ class Robot():
         self.d_geometric = d_geometric
         self.alpha = alpha
         self.friction=friction
+        self.q_r = q_r
+        self.q_rdot = q_rdot
+        self.d1 = d1
+        self.d2 = d2
+        self.S3 = S3
+        self.r_odo = r_odo
         
         self.M_r = np.diag([self.m,self.m,self.I_z])
         self.M_w = np.diag(self.I_w)
@@ -39,11 +45,19 @@ class Robot():
         if self.alpha==None:
             self.alpha = [0.25 * np.pi, -0.25 * np.pi, -0.25 * np.pi, 0.25 * np.pi]
         
+        if self.q_r is None:
+            self.q_r = np.array([[0],[0],[0]])
+        if self.q_rdot is None:
+            self.q_rdot = np.array([[0],[0],[0]])
+        
         self.x_center = x_center
         self.y_center = y_center
         
         self.S = self.S_geometric - self.x_center
         self.d = self.d_geometric - self.y_center
+        
+        self.q_d = np.zeros((3,1))
+        self.R_d = 1./self.r_odo * np.array([[1,0,-self.d1],[1,0,-self.d2],[0,1,self.S3]])
         
         self.R = np.zeros((4,3))
         for i in range(4):
@@ -64,49 +78,28 @@ class Robot():
     def K_div_psidot_analytic(self):
         return np.matrix([[0,self.I_w[0] * 4 * self.r**-2 ,0],[-self.I_w[0] * 4 * self.r**-2,0,0],[0,0,0]])
     
-    def time_integrate(self,q_r0, q_rdot0, Gamma, N, dt=1E-2):
-        self.N = N
-        self.dt = dt
-        self.t = np.arange(0, self.N*self.dt, self.dt)
-        self.q_r = np.zeros((self.N,3,1))
-        self.q_rdot = np.zeros((self.N,3,1))
-        self.q_r[0] = q_r0
-        self.q_rdot[0] = q_rdot0
+    def get_aceleration(self, Gamma):
+        Rotation = self.rotationmatrix(self.q_r[2,0])
+        Rotationdot = self.rotationmatrixdot(self.q_r[2,0],self.q_rdot[2,0])
         
-        for i in range(1,N):
-            Rotation = self.rotationmatrix(self.q_r[i-1,2,0])
-            Rotationdot = self.rotationmatrixdot(self.q_r[i-1,2,0],self.q_rdot[i-1,2,0])
-            
-            q_wdot = self.R @ Rotation.T @ self.q_rdot[i-1]
-            
-            self.H = self.M_r + Rotation @ self.R.T @ self.M_w @ self.R @ Rotation.T
-            self.K = Rotation @ self.R.T @ self.M_w @ self.R @ Rotationdot.T
-            self.F_a = Rotation @ self.R.T @ (Gamma[i-1] - q_wdot * self.friction)
-            
-            self.q_rddot = np.linalg.inv(self.H) @ (self.F_a - self.K @ self.q_rdot[i-1])
-            self.q_rdot[i] = self.q_rdot[i-1] + self.q_rddot * self.dt
-            self.q_r[i] = self.q_r[i-1] + self.q_rdot[i-1] * self.dt
+        q_wdot = self.R @ Rotation.T @ self.q_rdot
         
-    def simulate_odometry(self,d1=0.1,d2=-0.1,S3=-0.1,r_odo=0.05):
-        self.d1 = d1
-        self.d2 = d2
-        self.S3 = S3
-        self.r_odo = r_odo
-        
-        self.R_d = 1./self.r_odo * np.array([[1,0,-d1],[1,0,-d2],[0,1,S3]])
-        self.q_d = np.zeros((self.N,3,1))
-        
-        for i in range(1,self.N):
-            self.q_d[i] = self.q_d[i-1] + self.dt * (self.R_d @ (self.rotationmatrix(self.q_r[i-1,2,0]).T @ self.q_rdot[i-1]))
+        self.H = self.M_r + Rotation @ self.R.T @ self.M_w @ self.R @ Rotation.T
+        self.K = Rotation @ self.R.T @ self.M_w @ self.R @ Rotationdot.T
+        self.F_a = Rotation @ self.R.T @ (Gamma - q_wdot * self.friction)
+        q_rddot = np.linalg.inv(self.H) @ (self.F_a - self.K @ self.q_rdot)
+        return q_rddot
     
-    # def predict_position_from_odometry(self, q_d_ext):
-    #     N = q_d_ext.shape[0]
-    #     q_r_predict = np.zeros((N,3,1))
-    #     for i in range(1,N):
-    #         dq_d = q_d_ext[i] - q_d_ext[i-1]
-    #         dq_r = self.rotationmatrix(q_r_predict[i-1,2,0]) @ (np.linalg.inv(self.R_d) @ dq_d) 
-    #         q_r_predict[i] = q_r_predict[i-1] + dq_r
-    #     return q_r_predict
+    def time_integrate(self, Gamma, dt=1E-2):
+        q_rddot = self.get_aceleration(Gamma) #forward euler method
+        self.q_rdot = self.q_rdot + q_rddot * dt
+        self.q_r = self.q_r + self.q_rdot * dt
+        
+    def get_odometry_vel(self, q_r, q_rdot):
+        return self.R_d @ (self.rotationmatrix(q_r[2,0]).T @ q_rdot[i-1])
+    
+    def simulate_odometry(self, q_r, q_rdot, dt=1E-2):
+        self.q_d = self.q_d + self.get_odometry_vel(q_r, q_rdot) * dt
     
     def predict_position_from_odometry(self, t_ext, q_d_funct,x0 = np.zeros((3,1))):
         N = t_ext.shape[0]
@@ -118,35 +111,35 @@ class Robot():
             q_r_predict[i] = q_r_predict[i-1] + dq_r
         return q_r_predict
     
-    def predict_position_from_odometry_fancy(self, t_ext, q_d_funct,x0 = np.zeros((3,1))):
-        N = t_ext.shape[0]
-        q_r_predict = np.zeros((N,3,1))
-        q_r_predict[0] = x0
-        for i in range(1,N):
-            dt = t_ext[i]-t_ext[i-1]
-            print("time:")
-            print(t_ext[i])
-            print("dt:")
-            print(dt)
-            q_rdot = (np.linalg.inv(self.R_d) @ q_d_funct(t_ext[i])) - (np.linalg.inv(self.R_d) @ q_d_funct(t_ext[i-1]))
-            psidot = q_rdot[2]
-            print('q_rdot is')
-            print(q_rdot)
-            posdot = np.array([[q_rdot[0,0]],[q_rdot[1,0]]])
-            print('posdot is')
-            print(posdot)
-            integration_matrix = np.zeros((2,2))
-            if psidot == 0:
-                integration_matrix = np.identity(2) * dt
-            else:
-                integration_matrix = np.array([[np.sin(psidot),np.cos(psidot)-1],[1-np.cos(psidot),np.sin(psidot)]])[:,:,0]*dt/psidot
-            print("integration matrix is")
-            print(integration_matrix)
-            print(integration_matrix.shape)
-            dpos = self.rotationmatrixsmall(q_r_predict[i-1,2,0]) @ integration_matrix @ posdot
-            dq_r = np.array([[dpos[0,0]],[dpos[1,0]],[psidot*dt]])
-            q_r_predict[i] = q_r_predict[i-1] + dq_r
-        return q_r_predict
+    # def predict_position_from_odometry_fancy(self, t_ext, q_d_funct,x0 = np.zeros((3,1))):
+    #     N = t_ext.shape[0]
+    #     q_r_predict = np.zeros((N,3,1))
+    #     q_r_predict[0] = x0
+    #     for i in range(1,N):
+    #         dt = t_ext[i]-t_ext[i-1]
+    #         print("time:")
+    #         print(t_ext[i])
+    #         print("dt:")
+    #         print(dt)
+    #         q_rdot = (np.linalg.inv(self.R_d) @ q_d_funct(t_ext[i])) - (np.linalg.inv(self.R_d) @ q_d_funct(t_ext[i-1]))
+    #         psidot = q_rdot[2]
+    #         print('q_rdot is')
+    #         print(q_rdot)
+    #         posdot = np.array([[q_rdot[0,0]],[q_rdot[1,0]]])
+    #         print('posdot is')
+    #         print(posdot)
+    #         integration_matrix = np.zeros((2,2))
+    #         if psidot == 0:
+    #             integration_matrix = np.identity(2) * dt
+    #         else:
+    #             integration_matrix = np.array([[np.sin(psidot),np.cos(psidot)-1],[1-np.cos(psidot),np.sin(psidot)]])[:,:,0]*dt/psidot
+    #         print("integration matrix is")
+    #         print(integration_matrix)
+    #         print(integration_matrix.shape)
+    #         dpos = self.rotationmatrixsmall(q_r_predict[i-1,2,0]) @ integration_matrix @ posdot
+    #         dq_r = np.array([[dpos[0,0]],[dpos[1,0]],[psidot*dt]])
+    #         q_r_predict[i] = q_r_predict[i-1] + dq_r
+    #     return q_r_predict
     
     def control_heading_to_torque(self, q_rddot_heading):
         return self.R @ q_rddot_heading
@@ -162,7 +155,7 @@ class Robot():
         output[4] = pos + rotation @ np.array([[self.L],[self.l]])
         return output
    
-    def plot_evolution(self, fig=None, block=False, show=True):
+    def plot_evolution(self, t, q_r, q_rdot, fig=None, block=False, show=True):
         if fig==None:
             fig, (ax1, ax2) = plt.subplots(2)
         elif len(fig.axes) < 2:
@@ -174,30 +167,30 @@ class Robot():
             ax1 = fig.axes[0]
             ax2 = fig.axes[1]
         
-        ax1.plot([0, np.max(self.t)],[0,0],'k')
-        ax1.plot(self.t, self.q_r[:,0,0],'b', label='X position')
-        ax1.plot(self.t, self.q_r[:,1,0],'r', label='Y position')
-        ax1.plot(self.t, self.q_r[:,2,0],'g', label='Psi position')
+        ax1.plot([0, np.max(t)],[0,0],'k')
+        ax1.plot(t, q_r[:,0,0],'b', label='X position')
+        ax1.plot(t, q_r[:,1,0],'r', label='Y position')
+        ax1.plot(t, q_r[:,2,0],'g', label='Psi position')
         ax1.legend()
         ax1.title.set_text('Mechanum Wheeled Robot')
         
-        ax2.plot([0, np.max(self.t)],[0,0],'k')
-        ax2.plot(self.t, self.q_rdot[:,0,0],'b', label='X velocity')
-        ax2.plot(self.t, self.q_rdot[:,1,0],'r', label='Y velocity')
-        ax2.plot(self.t, self.q_rdot[:,2,0],'g', label='Psi velocity')
+        ax2.plot([0, np.max(t)],[0,0],'k')
+        ax2.plot(t, q_rdot[:,0,0],'b', label='X velocity')
+        ax2.plot(t, q_rdot[:,1,0],'r', label='Y velocity')
+        ax2.plot(t, q_rdot[:,2,0],'g', label='Psi velocity')
         ax2.legend()
         ax2.set_xlabel('t')
         if show:
             plt.show(block=block)
     
-    def plot_trajectory(self, fig=None, block=False, drawrobot=True, show=True, linecolor='b', label = None):
+    def plot_trajectory(self, q_r, fig=None, block=False, drawrobot=True, show=True, linecolor='b', label = None):
         if fig==None:
             fig = plt.figure()
         else:
             plt.figure(fig.number)
         
-        xPos = self.q_r[:,0,0]
-        yPos = self.q_r[:,1,0]
+        xPos = q_r[:,0,0]
+        yPos = q_r[:,1,0]
 
         maxd = np.max(np.abs(np.array([xPos,yPos])))
         
@@ -212,27 +205,46 @@ class Robot():
         plt.ylabel('Y')
         plt.title('Robot Trajectory')
         if drawrobot:
-            plt.plot(self.get_robot_outline(self.q_r[0])[:,0],self.get_robot_outline(self.q_r[0])[:,1],"purple")
-            plt.plot(self.get_robot_outline(self.q_r[-1])[:,0],self.get_robot_outline(self.q_r[-1])[:,1],"red")
+            plt.plot(self.get_robot_outline(q_r[0])[:,0], self.get_robot_outline(q_r[0])[:,1],"purple")
+            plt.plot(self.get_robot_outline(q_r[-1])[:,0], self.get_robot_outline(q_r[-1])[:,1],"red")
         plt.gca().set_aspect('equal')
         if show:
             plt.show(block=block)
+    
+    def get_state(self):
+        return np.array([[self.q_r],[self.q_rdot]])
+    
+    def to_string(self):
+        return f'Position {self.q_r}, Velocity {self.q_rdot}'
 
 if __name__ == "__main__":
-    robot = Robot()
-    
-    N = int(1E3)
-    Gamma = np.zeros((N,4,1))
-    Gamma[:,0,0] = 2
-    Gamma[:,1,0] = 1
-    Gamma[:,2,0] = 2
-    Gamma[:,3,0] = 1
     q_r0 = np.array([[0],[0],[0]])
     q_rdot0 = np.array([[1],[1],[2]])
+    
+    robot = Robot(q_r=q_r0, q_rdot=q_rdot0)
+    
+    N = int(1E3)
+    dt = 1E-2
+    t = np.arange(0, N*dt, dt)
+    
+    Gamma = np.zeros((4,1))
+    Gamma[0,0] = 2
+    Gamma[1,0] = 1
+    Gamma[2,0] = 2
+    Gamma[3,0] = 1
+    
+    q_r = np.zeros((N,3,1))
+    q_rdot = np.zeros((N,3,1))
+    q_r[0] = q_r0
+    q_rdot[0] = q_rdot0
+    
+    for i in range(N):
+        robot.time_integrate(Gamma, dt=dt)
+        q_r[i] = robot.q_r
+        q_rdot[i] = robot.q_rdot
     
     fig1, axis = plt.subplots(2)
     fig2=plt.figure()
     
-    robot.time_integrate(q_r0, q_rdot0, Gamma, N)
-    robot.plot_evolution(fig=fig1)
-    robot.plot_trajectory(fig=fig2)
+    robot.plot_evolution(t, q_r, q_rdot, fig=fig1)
+    robot.plot_trajectory(q_r, fig=fig2)
