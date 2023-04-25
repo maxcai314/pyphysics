c20ed05bd3eb3eb589f11f5ee155c5bf8d99614d
Max Cai <maxster@xz.ax>
2023-04-25 15:09:34 -0700

    purge of old files

--- Mecanum_Drive/test_read_mpc_log.py
+++
@@ -1,7 +0,0 @@
-from Mecanum_Drive.drive_log_reader import DriveLogReader
-
-simulation = DriveLogReader('drive_samples/mpc_log_1.csv')
-simulation.simulate_from_logs(time_step=1E-2, angle_wrap=False,relative_vel=True)
-simulation.plot_evolution(legends = True)
-simulation.plot_trajectory()
-simulation.plot_acceleration()
